68b3c7ca028b2525ea6647ffb4c6a1ca47e036e5
Jerick Son Icamina <jerickson.icamina@Jericks-MacBook-Air.local>
2022-06-10 07:26:42 +0800

    Added controller and setup make file

---
+++ Makefile
@@ -0,0 +1,19 @@
+CONTAINER := playing-cards-service
+
+start:
+	docker-compose up -d
+
+stop:
+	docker-compose down --remove-orphans
+
+ssh:
+	docker exec -it ${CONTAINER} /bin/bash
+
+composer-install:
+	docker exec ${CONTAINER} bash -c "composer require $(package)"
+
+clear-cache:
+	docker exec ${CONTAINER} bash -c "php bin/console cache:clear"
+
+create-controller:
+	docker exec ${CONTAINER} bash -c "php bin/console make:controller $(name)"
---
+++ src/Controller/CardController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardController extends AbstractController
+{
+    #[Route('/card', name: 'app_card')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CardController.php',
+        ]);
+    }
+}
